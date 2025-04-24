dc3744de48cd62c2d5b2f1d38aac7a6c1780c1bc
Sergio Hervás <sergiohervas18@gmail.com>
2025-04-25 01:08:26 +0200

    Reset

---
+++ Practicas/P3/elements/gancho_ani.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float):
+	if Input.is_action_pressed("espacio"):
+		$AnimationPlayer.play("Curva_Bezier_001_acción_001")
+	if Input.is_action_just_released("espacio"):
+		$AnimationPlayer.pause()
+	pass
