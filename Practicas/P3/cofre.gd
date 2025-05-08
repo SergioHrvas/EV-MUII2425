2c0c245b4cc0c04be268a5fbaa2236ad81f9f73a
Sergio Hervás <sergiohervas18@gmail.com>
2025-05-08 10:53:46 +0200

    Materials added

--- Practicas/P3/cofre.gd
+++ Practicas/P3/cofre.gd
@@ -4,6 +4,3 @@ extends Node3D
 func _process(delta: float):
 	if Input.is_action_just_pressed("espacio"):
 		$AnimationPlayer.play("Scene")
-	
-	if Input.is_action_just_released("espacio"):
-		$AnimationPlayer.pause()  # Usamos stop() en lugar de pause()
